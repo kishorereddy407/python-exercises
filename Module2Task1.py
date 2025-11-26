ab89b9f3ae787392f44124dbaca46d5066d338bb
Kishoreplsind_123 <kishore.plsind@in.panasonic.com>
2025-11-26 12:33:13 +0530

    Task1 and Task2 for Module2

---
+++ Module2Task1.py
@@ -0,0 +1,12 @@
+input1 = int(input("Enter the first number: "))
+input2 = int(input("Enter the second number: "))
+print() # just to create an empty line between input and output
+
+addition = input1 + input2
+substraction = input1 - input2
+multiplication = input1 * input2
+division = input1 / input2
+print("Addition:",addition)
+print("Substraction:",substraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
---
+++ Module2Task2.py
@@ -0,0 +1,6 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+lastName = lastName + "!"
+print() # just to create an empty line between input and output
+
+print("Hello,",firstName,lastName,"Welcome to the python program")
